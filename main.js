4d97e97241c38156ef0b44394595c297c7a52b06
Erik Grinn <grinn.erik@gmail.com>
2024-08-14 16:45:47 -0400

    user input

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello worl')
+
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return choices[randomIndex]
+    // return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice() {
+    const choices = ['rock', 'paper', 'scissors']
+    const userChoice = String(prompt('enter your choice'))
+    
+}
+
+console.log(getComputerChoice())
